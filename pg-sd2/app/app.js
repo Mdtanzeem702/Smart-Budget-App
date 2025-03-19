002ce476e13315ff790cfb1af43d40a4a09e72b7
Mdtanzeem702 <mdtanzeemnoori@gmail.com>
2025-03-19 17:55:53 +0530

    massage

--- pg-sd2/app/app.js
+++ pg-sd2/app/app.js
@@ -6,13 +6,15 @@ var app = express();
 
 // Add static files location
 app.use(express.static("static"));
+app.set('view engine', 'pug');
+app.set('views', './app/views');
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    res.render("index");
 });
 
 // Create a route for testing the db
